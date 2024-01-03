4452cda53d0763ee5a263c9bec04d64dea02ab2b
Emily Krohn <emilykrohn2@gmail.com>
2024-01-03 11:00:49 -0800

    Stop in from of refrigerator and shelf
    Customer1 stops in from of refrigerator and shelf

--- Scripts/Customers/customer_1.gd
+++ Scripts/Customers/customer_1.gd
@@ -1,12 +1,8 @@
 extends RigidBody2D
 
-func _on_area_2d_body_entered(body):
-	get_parent().can_move = false
 
 
-func _on_area_2d_body_exited(body):
-	get_parent().can_move = true
-
-
-func _on_area_2d_area_entered(area):
-	get_parent().can_move = false
+func _on_object_detector_area_entered(area):
+	print(area)
+	if area.name == "Object":
+		get_parent().can_move = false
