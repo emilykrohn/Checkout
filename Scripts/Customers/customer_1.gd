95187d49ea3638ce94138e59f47760bcb93c2c2c
Emily Krohn <emilykrohn2@gmail.com>
2024-01-03 11:03:26 -0800

    Stop and then move again after 0.5 seconds
    Character1 stops at each shelf and refrigerator and then starts moving again after 0.5 seconds

--- Scripts/Customers/customer_1.gd
+++ Scripts/Customers/customer_1.gd
@@ -3,6 +3,10 @@ extends RigidBody2D
 
 
 func _on_object_detector_area_entered(area):
-	print(area)
 	if area.name == "Object":
 		get_parent().can_move = false
+		$Timer.start()
+
+
+func _on_timer_timeout():
+	get_parent().can_move = true
